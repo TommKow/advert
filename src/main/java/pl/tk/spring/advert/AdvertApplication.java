6cb8b55f408d2718a60566ebd8c7777c004eb8dc
Tomasz Kowalski <tomasz.kowalski87@yahoo.com>
2019-07-18 21:15:55 +0200

    App

---
+++ src/main/java/pl/tk/spring/advert/AdvertApplication.java
@@ -0,0 +1,13 @@
+package pl.tk.spring.advert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvertApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdvertApplication.class, args);
+	}
+
+}
