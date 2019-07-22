d238f3ddf933b85f36057b34507b63f45b5b1f95
Tomasz Kowalski <tomasz.kowalski87@yahoo.com>
2019-07-22 21:57:18 +0200

    HomePage with AdvertList - Added

---
+++ src/main/java/pl/tk/spring/advert/controllers/HomePageController.java
@@ -0,0 +1,25 @@
+package pl.tk.spring.advert.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import pl.tk.spring.advert.domain.repositories.AdvertRepository;
+
+@Controller
+@RequestMapping("/")
+public class HomePageController {
+
+    private AdvertRepository advertRepository;
+    @Autowired
+    public HomePageController(AdvertRepository advertRepository) {
+        this.advertRepository = advertRepository;
+    }
+
+    @GetMapping
+    public String prepareHomePage(Model model) {
+        model.addAttribute("AdvertList", advertRepository.findAllByOrderByPostedDesc());
+        return "";
+    }
+}
--- src/main/java/pl/tk/spring/advert/domain/repositories/AdvertRepository.java
+++ src/main/java/pl/tk/spring/advert/domain/repositories/AdvertRepository.java
@@ -5,7 +5,10 @@ import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
 import pl.tk.spring.advert.domain.model.Advert;
 
+import java.util.List;
+
 @Repository
 public interface AdvertRepository extends JpaRepository<Advert, Long> {
 
+    List<Advert> findAllByOrderByPostedDesc();
 }
